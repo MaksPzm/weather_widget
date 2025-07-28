4ab83d947c0cf99244dceb5077c826545365b050
MaksPzm <makspzm@gmail.com>
2025-07-29 01:20:56 +0500

    new

---
+++ src/components/app/App.tsx
@@ -0,0 +1,25 @@
+import './index.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <section className='wrapper'>
+        <header className='header'>
+
+        </header>
+        <aside>
+
+        </aside>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
+
+      </section>
+    </div>
+  );
+}
+
+export default App;
